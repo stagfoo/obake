d0c74033e0eb153984c6a427ab15ec38e70a1633
Alex <alexanderddking@gmail.com>
2019-06-18 16:38:18 +0900

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # obake (ﾉ⊙﹏⊙)ﾉ
 exploring statement management with proxies and promise...in a spooky way
+
+## Getting started
+
+```js
+// the defaultState is the state you want to proxy
+const defaultState = {
+ greeting: 'BoOOOoo!'
+}
+const render = () => {
+  //re-render your app like react, vue or lit
+}
+const reducers {
+  CHANGE_GREETING: (state, propName, value) => {
+    //Do something to the state
+    state.greeting = value;
+    return new Promise(function(resolve, reject) { resolve(state) });
+  }
+}
+const state = createStore(defaultState, render, reducers);
+
+```
+Because its a proxy it means you set it like a normal object when you want to change the state
+
+```js
+
+state.greeting = {
+  name: 'CHANGE_GREETING',
+  value: 'HelloooOOOoo!'
+}
+
+```
+as you can see however it takes an reducer name which must be registered in the reducers,
+this way you can just mutate the object willy nilly.
+
+and there you go;
+you have reducers, you have immutable state and you have a safe way to mutate it.
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 export function createStore(defaultState: object, render: Function, reducers: object) {
   return new Proxy(defaultState, {
-  set(target, prop, value) {
-    // Check the reducer is real
-    if(Object.keys(reducers).indexOf(value.action) > -1){
-      return reducers[value.action](target, prop, value).then(res => {
-        // This actually sets the props on the proxy object
-        Reflect.set(target, prop, res[prop]);
-        // Re-render the app
-      }).then(() => {
-        render();
-        return true
-      });
-      ;
-    } else {
-      console.error('This is not a valid reducer, ya turkey 🦃')
-      return false
+    set(target, prop, action) {
+      // Check the reducer is real
+      if (Object.keys(reducers).indexOf(action.name) > -1) {
+        return reducers[action.name](target, prop, action.value).then(res => {
+          // This actually sets the props on the proxy object
+          Reflect.set(target, prop, res[prop]);
+          // Re-render the app
+        }).then(() => {
+          render();
+          return true
+        });
+        ;
+      } else {
+        console.error('This is not a valid reducer, ya turkey 🦃')
+        return false
+      }
     }
-  }
-});
+  });
 }
