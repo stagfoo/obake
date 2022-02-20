4965bbd118e56602d05b2c233a49a42c400ed63f
stagfoo <alexanderddking@gmail.com>
2022-02-20 23:58:01 +0900

    🍱　refactor ░ add better typing

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,60 @@
-export function createStore(defaultState: object, watchers: object, reducers: object) {
-  let proxyState;
-  defaultState['_update'] = (name, payload) => {
+
+type Update = (_name: string, payload: unknown) => void
+
+interface ProxiedObject {
+  [k:string | symbol]: any,
+  _update?: Update
+  _?: { name:string, value:unknown }
+}
+
+type MutationFunction = (state: ProxiedObject, value: unknown) => void;
+type Reducer = (state: ProxiedObject, value: unknown) => Promise<ProxiedObject>;
+type Watcher = (state: ProxiedObject) => void;
+
+type Watchers = {
+  [k:string]: Watcher
+}
+type Reducers = {
+  [k:string]: Reducer
+}
+
+export function createStore(defaultState: ProxiedObject, watchers: Watchers, reducers: Reducers) {
+  let proxyState: ProxiedObject;
+  defaultState._update = (name, payload) => {
     proxyState._ = {
       name,
-      value: payload
+      value: payload,
     };
   };
+
   proxyState = new Proxy(defaultState, {
-    set(target, prop, action) {
+    // TODO: internal type isn't accurate I think
+    set(target, prop, action): any {
       // Check the reducer is real
       if (Object.keys(reducers).indexOf(action.name) > -1) {
         return reducers[action.name](target, action.value)
-          .then(res => {
+          .then((state: ProxiedObject) => {
             // This actually sets the props on the proxy object
-            Reflect.set(target, prop, res[prop]);
-            // Re-render the app
-          })
-          .then(() => {
-            //render the newState
-            Object.keys(watchers).map(key => watchers[key](target))
+            Reflect.set(target, prop, state[prop]);
+            // Render the newState
+            Object.keys(watchers).map(key => watchers[key](target));
             return true;
+            // Re-render the app
           });
-      } else {
-        throw new Error("This is not a valid reducer, (ﾉ⊙﹏⊙)ﾉ");
-        return false;
       }
-    }
+
+      throw new Error('This is not a valid reducer, (ﾉ⊙﹏⊙)ﾉ');
+    },
   });
   return proxyState;
 }
 
 // Optional Reducer function
-export function reducer(mutation){
-  return function (state, value) {
-    mutation(state, value)
-    return new Promise(function(resolve, reject) {
+export function reducer(mutation: MutationFunction): Reducer {
+  return function (state: ProxiedObject, value: unknown) {
+    mutation(state, value);
+    return new Promise(resolve => {
       resolve(state);
     });
-  }
+  };
 }
